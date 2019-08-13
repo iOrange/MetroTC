507afad1d12c7e98fdb31473bfb059df1688c445
iOrange <sergey.kudlay@hotmail.com>
2019-08-12 20:33:51 -0400

    + Added files that I forgot to include in my previous commit

---
+++ src/Hyperlinks.h
@@ -0,0 +1,12 @@
+// Hyperlinks.h
+//
+// Copyright 2002 Neal Stublen
+// All rights reserved.
+//
+// http://www.awesoftware.com
+//
+
+#include <Windows.h>
+
+BOOL ConvertStaticToHyperlink(HWND hwndCtl);
+BOOL ConvertStaticToHyperlink(HWND hwndParent, UINT uiCtlId);
